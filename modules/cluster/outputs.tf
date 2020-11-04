5756c9bbe5ad98d3fa1a6473d12595c6d96b47c5
Sapit, Jonalyn M <jonalyn.m.sapit@accenture.com>
2020-11-05 00:20:06 +0800

    third

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -13,10 +13,10 @@ output "certificate" {
   description = "Certificate used to authenticate to the Kubernetes Controle Plane."
 }
 
-# output "security_group" {
-#   value       = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
-#   description = "Certificate used to authenticate to the Kubernetes Controle Plane."
-# }
+output "security_group" {
+  value       = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
+  description = "Certificate used to authenticate to the Kubernetes Controle Plane."
+}
 
 # output "node_role" {
 #   value       = aws_iam_role.node.name
